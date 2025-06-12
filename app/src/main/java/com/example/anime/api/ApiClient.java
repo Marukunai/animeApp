80e83cfb4f552c295c4b572d3e52a9a616d7efda
Marukunai03 <102300244+Marukunai@users.noreply.github.com>
2025-06-12 16:15:44 +0200

    comprobados fallos en las paginas, ejecucion login

--- app/src/main/java/com/example/anime/api/ApiClient.java
+++ app/src/main/java/com/example/anime/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://TU_IP:PUERTO/"; // Cambia esto
+    private static final String BASE_URL = "http://localhost:8080/"; // Cambia esto
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
